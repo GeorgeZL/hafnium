6e7abd3d0d709d63b1286b65290c3979ee0e8825
George Zhou <george.l.zhou@gmail.com>
2022-11-12 09:20:00 +0800

    gic update

--- src/drivers/gic/vgic.c
+++ src/drivers/gic/vgic.c
@@ -1,63 +1,32 @@
+#include "hf/device/vgic.h"
 #include "hf/device/gic.h"
-
-#if GIC_VERSION == 3 || GIC_VERSION ==4
-#include "gicv3.h"
-#elif GIC_VERSION == 2
+#include "hf/mm.h"
 #include "gicv2.h"
-#endif
 
-#if GIC_VERSION == 3 || GIC_VERSION ==4
-#if 0
-int vgicv3_init(struct vm *vm, struct mpool *ppool);
-int gicv3_init(void);
-int gicv3_secondary_init(void);
-#else
-int vgicv3_init(struct vm *vm, struct mpool *ppool)
-{
-    return 0;
-}
-
-int gicv3_init(void)
-{
-    return 0;
-}
-
-int gicv3_secondary_init(void)
-{
-    return 0;
-}
-
-#endif
-#elif GIC_VERSION == 2
 int vgicv2_init(struct vm *vm, struct mpool *ppool);
 int gicv2_init(void);
 int gicv2_secondary_init(void);
-#endif
+void gicv2_mm_init(
+    struct mm_stage1_locked stage1_locked, struct mpool *ppool);
 
 int vgic_init(struct vm *vm, struct mpool *ppool)
 {
-#if GIC_VERSION == 3 || GIC_VERSION ==4
-    return vgicv3_init(vm, ppool);
-#elif GIC_VERSION == 2
     return vgicv2_init(vm, ppool);
-#endif
 }
 
 int gic_init(void)
 {
-#if GIC_VERSION == 3 || GIC_VERSION ==4
-    return gicv3_init();
-#elif GIC_VERSION == 2
     return gicv2_init();
-#endif
 }
 
 int gic_secondary_init(void)
 {
-#if GIC_VERSION == 3 || GIC_VERSION ==4
-    return gicv3_secondary_init();
-#elif GIC_VERSION == 2
     return gicv2_secondary_init();
-#endif
 }
 
+void gic_mm_init(
+    struct mm_stage1_locked stage1_locked, struct mpool *ppool)
+{
+	/* Map page for gic */
+    gicv2_mm_init(stage1_locked, ppool);
+}
